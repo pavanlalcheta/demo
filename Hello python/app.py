6b265f363b48cd7f879158ca5bb09d236bbc3624
pavanlalcheta <pavanlalcheta09@gmail.com>
2025-10-09 10:08:54 +0530

    python/app.py updated

--- Hello python/app.py
+++ Hello python/app.py
@@ -1,10 +1,32 @@
+from datetime import datetime
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+Data_dir = '/app/data'
+myfile = os.path.join(Data_dir,'greeting_log.txt')
+
+if not os.path.exists(Data_dir):
+    os.makedirs(Data_dir)
+
 @app.route('/')
 def hello():
-    return "hello Docker"
+    # return "hello Docker"
+    mesaage = 'hello docker'
+    timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+
+    with open(myfile,'a') as f:
+        f.write(f"{timestamp}:{mesaage}\n")
+
+    log_content = ''
+    try:
+        with open(myfile,'r') as f:
+            log_content = f.read()
+    except FileNotFoundError:
+        log_content = "No Prviouse Log Found..!"
+
+    return f"Log:<br><pre>{log_content}</pre>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
